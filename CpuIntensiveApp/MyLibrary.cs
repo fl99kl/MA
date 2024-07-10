fbe097152fb22efb4892702626ee68b1cb9e7a3d
fl99kl <floriankleinert99@web.de>
2024-07-10 14:15:24 +0000

    reworked app to work under linux

--- CpuIntensiveApp/MyLibrary.cs
+++ CpuIntensiveApp/MyLibrary.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 public static class MyLibrary
 {
-    [DllImport("D:\\MA\\CpuIntensiveApp\\libmyclibrary.so", EntryPoint = "hello_from_c", CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("libs/libmylibrary.so", EntryPoint = "hello_from_c", CallingConvention = CallingConvention.Cdecl)]
     public static extern void HelloFromC();
 }
