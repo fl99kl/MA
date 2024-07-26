4ba58839675764e008805c9bf56ff457717c0a01
fl99kl <floriankleinert99@gmail.com>
2024-07-26 12:52:45 +0200

    change entrypoint name

--- CpuIntensiveApp/PapiWrapper.cs
+++ CpuIntensiveApp/PapiWrapper.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 public static class PapiWrapper
 {
-    [DllImport("papi_wrapper.so", EntryPoint = "initialize_papi")]
+    [DllImport("papi_wrapper.so", EntryPoint = "runRapl")]
     public static extern void runRapl();
 
 }
