876ccf8b425e60afc3b0a325dfc02ec271526467
fl99kl <floriankleinert99@gmail.com>
2024-09-16 11:42:48 +0200

    remove timestamp

--- CpuIntensiveApp/add_tsdb_entry.c
+++ CpuIntensiveApp/add_tsdb_entry.c
@@ -1,7 +1,6 @@
 ﻿#include <stdio.h>
 #include <string.h>
 #include <curl/curl.h>
-#include <sys/time.h>
 
 const char *url = "http://localhost:8086/api/v2/write";
 const char *bucket = "myBucket";
@@ -12,15 +11,8 @@ char *key = "sensor1";
 float value1 = 23.5;
 float value2 = 45.6;
 
-
-
-// Get the current time in seconds and microseconds
-struct timeval tv;
-gettimeofday(&tv, NULL);
-long long timestamp = (long long)tv.tv_sec * 1000000000LL + (long long)tv.tv_usec * 1000LL;
-
 char data[256];
-snprintf(data, sizeof(data), "my_measurement,key=%s value1=%f,value2=%f %lld", key, value1, value2, timestamp);
+snprintf(data, sizeof(data), "my_measurement,key=%s value1=%f,value2=%f", key, value1, value);
 
 CURL *curl;
 CURLcode res;
