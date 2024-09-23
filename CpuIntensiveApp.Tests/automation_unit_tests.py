2d1e25c1b0fdd517146901c0ab08031f4c92688f
fl99kl <floriankleinert99@gmail.com>
2024-09-23 19:23:21 +0200

    bugfix automation unit test script

--- CpuIntensiveApp.Tests/automation_unit_tests.py
+++ CpuIntensiveApp.Tests/automation_unit_tests.py
@@ -1,25 +1,20 @@
 ﻿import subprocess
 import time
 
-# Number of times to execute the C program
+# Number of times to execute the dotnet test command
 num_executions = 15
 
-# Path to the compiled C program
-program_path = "dotnet test"
+# Command to execute the dotnet test
+command = ["dotnet", "test"]
 
 # Time to sleep (in seconds) between executions
 sleep_duration = 10
 
-# Library path (can be customized as needed)
-ld_library_path = "/home/kleinert/papi/src/install/lib"
-
 for i in range(num_executions):
     print(f"Execution {i + 1}/{num_executions}")
 
-    # Execute the C program with sudo, env, and LD_LIBRARY_PATH
-    subprocess.run([
-        "sudo", "env", f"LD_LIBRARY_PATH={ld_library_path}", program_path
-    ])
+    # Execute the dotnet test command
+    subprocess.run(command)
 
     # Sleep for the specified duration between executions
     if i < num_executions - 1:
