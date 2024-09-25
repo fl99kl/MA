8d138c7e298908963cc62633ddfbcc2811bb04b0
fl99kl <floriankleinert99@gmail.com>
2024-09-25 16:18:50 +0200

    bugfix plot function for other metrics than avg energy

--- CpuIntensiveApp/Plot-Data/plot_results_4_metric.py
+++ CpuIntensiveApp/Plot-Data/plot_results_4_metric.py
@@ -56,7 +56,7 @@ else:
     # Create a plot for each test_name
     for name, group in grouped:
         plt.plot(range(1, len(group) + 1), group[f'{metric}'], label=name)
-        print(f"y: {group['avg_energy_pkg']}")  # Print to check values
+        print(f"y: {group[f'{metric}']}")  # Print to check values
         print(f"x: {range(1, len(group) + 1)}")  # Print to check values
 
     # Adding labels and title
