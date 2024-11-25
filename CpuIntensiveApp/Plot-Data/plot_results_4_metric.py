6bc8e1cfd61ae40c70c218ac8d18573734a35b97
fl99kl <floriankleinert99@gmail.com>
2024-11-25 17:22:41 +0100

    bugfix excluded tests

--- CpuIntensiveApp/Plot-Data/plot_results_4_metric.py
+++ CpuIntensiveApp/Plot-Data/plot_results_4_metric.py
@@ -43,7 +43,7 @@ else:
     df = df.drop(columns=['_start', '_stop', '_measurement', '_time'], errors='ignore')
 
     # List of test names to exclude from the graph
-    excluded_tests = ["idle_consumption, SleepingTest", "test_to_exclude_2"]
+    excluded_tests = ['idle_consumption', 'SleepingTest']
 
     # Group by test_name
     grouped = df.groupby('test_name')
